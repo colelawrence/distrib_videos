e39fef26ad29b84c335ee29a3d458a2d49944cf2
Cole R Lawrence <msgzht@gmail.com>
2017-05-16 12:38:10 -0500

    doot doot

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule DistribVideos.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:edeliver, "~> 1.4.0"},
+     {:edeliver, "~> 1.4.2"},
      {:exrm, "~> 1.0.3"}]
   end
 
